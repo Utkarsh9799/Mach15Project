408ed731d4992eb51d5f3849ca2fa6bd7ee76b79
Utkarsh Kumar <utkarsh9799@gmail.com>
2019-08-30 21:45:46 +0530

    Routing of profile page and home page

--- website/src/App.js
+++ website/src/App.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
+import Home from './components/homepage/Home';
 import PP1 from './components/profilepage/PP1/PP1';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  render() {
+    return (
       <Router>
-        <PP1 />
+        <div className="App">
+          <Route exact path="/" render={props => (
+            <React.Fragment>
+              <Home />
+            </React.Fragment>
+          )} />
+          <Route path="/pp1" component={PP1} />
+        </div>
       </Router>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
